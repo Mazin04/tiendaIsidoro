354c71f76a2fc9e38da7c2c8a200fd5e26c80da0
Rubén García <rubengsegoviano@gmail.com>
2025-03-10 12:05:20 +0100

    Las traducciones funcionan, y configurada NoPage.jsx

--- src/components/languageSelect.jsx
+++ src/components/languageSelect.jsx
@@ -2,13 +2,21 @@ import { Listbox, ListboxButton, ListboxOption, ListboxOptions } from '@headless
 import { useState } from 'react'
 import { ReactCountryFlag } from 'react-country-flag'
 import { LANGUAGES } from '../constants'
+import { useTranslation } from 'react-i18next'
 
 const LanguageSelect = () => {
     const [selectedLanguage, setSelectedLanguage] = useState(LANGUAGES[0])
+    const { i18n, t } = useTranslation();
+
+    const onChangeLang = (selectedLang) => {
+        setSelectedLanguage(selectedLang);
+        console.log(selectedLang);
+        i18n.changeLanguage(selectedLang.code);
+    };
 
     return (
         <div className="relative w-fit flex justify-center items-center"> {/* Contenedor con relative */}
-            <Listbox value={selectedLanguage} onChange={setSelectedLanguage}>
+            <Listbox value={selectedLanguage} onChange={onChangeLang}>
                 <ListboxButton className="flex items-center justify-between w-full px-4 py-2 text-gray-700 bg-white border border-gray-300 rounded-lg shadow-sm focus:outline-none hover:bg-gray-100 transition duration-200">
                     <div className="flex items-center space-x-2">
                         <ReactCountryFlag countryCode={selectedLanguage.code} svg className="w-5 h-5" />
--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
-
+import { useTranslation } from "react-i18next";
 
 const NoPage = () => {
+    const { t } = useTranslation();
     return (
         <div className="flex flex-col items-center justify-center min-h-[600px] text-center">
             <h1 className="text-7xl font-bold text-[#004D43]">404</h1>
-            <p className="text-xl text-black mt-4">Oops, la página que buscas no existe.</p>
+            <p className="text-xl text-black mt-4">{t('404_message')}</p>
             <Link to={"/"} className="mt-6 px-6 py-3 bg-[#FFC107] text-black rounded-lg text-lg hover:bg-yellow-600 transition">
-                Volver al inicio
+                {t('404_return')}
             </Link>
         </div>
     );
